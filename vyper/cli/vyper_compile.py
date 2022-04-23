995bd8b512fd386984f0044eacca19d36a845554
Gary Tse <8017125+tserg@users.noreply.github.com>
2022-04-23 16:02:53 +0800

    add extended ast option

--- vyper/cli/vyper_compile.py
+++ vyper/cli/vyper_compile.py
@@ -29,6 +29,7 @@ devdoc             - Natspec developer documentation
 combined_json      - All of the above format options combined as single JSON output
 layout             - Storage layout of a Vyper contract
 ast                - AST in JSON format
+ast_extended       - Folded and type-annotated AST in JSON format
 interface          - Vyper interface of a contract
 external_interface - External interface of a contract, used for outside contract calls
 opcodes            - List of opcodes as a string
@@ -302,7 +303,13 @@ def compile_files(
         output_formats = combined_json_outputs
         show_version = True
 
-    translate_map = {"abi_python": "abi", "json": "abi", "ast": "ast_dict", "ir_json": "ir_dict"}
+    translate_map = {
+        "abi_python": "abi",
+        "json": "abi",
+        "ast": "ast_dict",
+        "ast_extended": "ast_extended_dict",
+        "ir_json": "ir_dict",
+    }
     final_formats = [translate_map.get(i, i) for i in output_formats]
 
     compiler_data = vyper.compile_codes(
